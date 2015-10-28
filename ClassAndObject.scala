319b56bc758058cbc570a23d5d040106d7ce6226
Jacobiann <s355338@gmail.com>
2015-10-28 11:43:16 +0800

    oo and Extends example

--- ClassAndObject.scala
+++ ClassAndObject.scala
@@ -13,6 +13,11 @@ object ClassAndObject {
     }
   }
 
+  //要overrride的變數必須是：1.immutable value(use val)
+  //2.只能繼承non-private的變數 3.副建構子無法extends
+  //In this case Point class is called "superclass"
+  //Location is called "subclass"
+  //Scala 只允許繼承一個Class
   class Location(override val xc: Int, override val yc:Int,
                  val zc:Int) extends Point(xc,yc){
     var z = zc
@@ -28,6 +33,8 @@ object ClassAndObject {
     val point = new Point(2,4)
     println("----Simple Example----")
     println(point.move(2,2))
+    println(point.x)
+    println(point.y)
     val loca = new Location(10,20,15)
     println("----Extends Example----")
     println(loca.move(2,3,4))
